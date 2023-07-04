9db1729e00deb35506c1084c16844dc9bdc5dc7a
nlemast <nlemaster47@gmail.com>
2023-07-04 10:23:52 -0700

    clean up file dirs, add chapter 01 and part of 03

---
+++ chapter_03/functions/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+    other_funcion();
+}
+
+fn other_funcion() {
+    println!("other function")
+}
---
+++ chapter_03/variables/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // let x = 5;
+    // let x = x + 1;
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is {x}");
+    // }
+    // println!("The value of x is {x}")
+
+    // let quotient = 56.7 / 32.2;
+    // let truncated = 5 / 3; // Results in -1
+    // println!("the quotient is {quotient}");
+    // println!("the truncated is {truncated}");
+
+    // let tup = (500, 6.4, 1);
+
+    // let last = tup.2;
+    // println!("tuppp, {last}")
+
+    // let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // let b: [i32; 5] = [3; 5]; // same as [3,3,3,3,3]
+}
